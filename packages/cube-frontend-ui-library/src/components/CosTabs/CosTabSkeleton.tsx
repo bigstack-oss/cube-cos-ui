ed632d80432875b32bd5289b8ac44ff3eac8c722
steven.chiu <steven.chiu@bigstack.co>
2025-01-15 08:27:27 +0800

    feat(ui-library): implement cos tabs

---
+++ packages/cube-frontend-ui-library/src/components/CosTabs/CosTabSkeleton.tsx
@@ -0,0 +1,9 @@
+import { CosSkeleton } from '../../internal/components/CosSkeleton/CosSkeleton'
+
+export const CosTabSkeleton = () => {
+  return (
+    <div className="px-5 py-2">
+      <CosSkeleton className="h-4 w-9" />
+    </div>
+  )
+}
