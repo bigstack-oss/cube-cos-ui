6349b14a24731d5eee6061b2218b887dbe1e02f1
Lizzy Liang <lizzy.liang@bigstack.co>
2025-01-17 15:58:08 +0800

    fix: comments

--- packages/cube-frontend-ui-library/src/components/CosHyperlink/CosHyperlink.tsx
+++ packages/cube-frontend-ui-library/src/components/CosHyperlink/CosHyperlink.tsx
@@ -1,5 +1,7 @@
-import classnames from 'classnames'
 import { IconProps } from '../Icon/iconUtils'
+import { createElement } from 'react'
+import { cva } from 'class-variance-authority'
+import { twMerge } from 'tailwind-merge'
 
 export type CosHyperlinkColor = 'primary' | 'secondary'
 
@@ -14,99 +16,95 @@ export type CosHyperlinkSize = 'sm' | 'md'
 type BaseHyperlinkProps = {
   color?: CosHyperlinkColor
   size: CosHyperlinkSize
-  text: string
+  disabled?: boolean
+  children: string
   href: string
 }
 
-type IconHyperlinkProps = {
-  variant: Extract<CosHyperlinkVariant, 'icon-left' | 'icon-right'>
-  Icon: React.FC<IconProps>
-}
-
-type TextHyperlinkProps = {
-  variant: Extract<CosHyperlinkVariant, 'text-only' | 'text-inline'>
-  Icon?: never
-}
-
 export type CosHyperlinkProps = BaseHyperlinkProps &
-  (IconHyperlinkProps | TextHyperlinkProps)
-
-const getClassNames = (
-  color: CosHyperlinkColor,
-  size: CosHyperlinkSize,
-  variant: CosHyperlinkVariant,
-) => {
-  const baseLinkClasses = 'flex items-center gap-x-1'
-
-  const colorLinkClasses: Record<CosHyperlinkColor, string> = {
-    primary: classnames(
-      'text-primary',
-      'hover:text-functional-hover-primary',
-      'visited:text-primary-700',
-      'disabled:text-functional-disable-text',
-    ),
-    secondary: classnames(
-      'text-functional-border-darker',
-      'hover:text-functional-text-light',
-      'visited:text-functional-border-darker',
-      'disabled:text-functional-disable-text',
-    ),
-  }
-
-  const sizeLinkClasses: Record<CosHyperlinkSize, string> = {
-    sm: 'primary-body4 font-medium',
-    md: 'primary-body2 font-medium',
-  }
-
-  const variantLinkClasses: Record<CosHyperlinkVariant, string> = {
-    'text-only': '',
-    'text-inline': 'underline underline-offset-4',
-    'icon-left': '',
-    'icon-right': '',
-  }
-
-  return classnames(
-    baseLinkClasses,
-    colorLinkClasses[color],
-    sizeLinkClasses[size],
-    variantLinkClasses[variant],
+  (
+    | {
+        variant: Extract<CosHyperlinkVariant, 'icon-left' | 'icon-right'>
+        Icon: React.ComponentType<IconProps>
+      }
+    | {
+        variant: Extract<CosHyperlinkVariant, 'text-only' | 'text-inline'>
+        Icon?: never
+      }
   )
-}
+
+const hyperlink = cva(['flex items-center gap-x-1 font-medium'], {
+  variants: {
+    color: {
+      primary: [
+        'text-primary',
+        'hover:text-functional-hover-primary',
+        'visited:text-primary-700',
+      ],
+      secondary: [
+        'text-functional-border-darker',
+        'hover:text-functional-text-light',
+        'visited:text-functional-border-darker',
+      ],
+    },
+    size: {
+      md: 'primary-body2',
+      sm: 'primary-body4',
+    },
+    variant: {
+      'text-only': '',
+      'text-inline': 'underline underline-offset-4',
+      'icon-left': '',
+      'icon-right': '',
+    },
+    disabled: {
+      true: 'text-functional-disable-text hover:cursor-default hover:text-functional-disable-text',
+    },
+  },
+})
 
 export const CosHyperlink = (props: CosHyperlinkProps) => {
   const {
     color = 'primary',
     size = 'md',
     variant = 'text-only',
-    text,
+    disabled = false,
+    children,
     href,
   } = props
 
-  const linkClasses = getClassNames(color, size, variant)
-
   const renderLeftIcon = () => {
-    const { Icon } = props
-    if (variant === 'icon-left' && Icon) {
-      return <Icon size={size} />
+    if (variant === 'icon-left') {
+      const { Icon } = props
+      return Icon && <Icon size={size} />
     }
-    return null
   }
 
   const renderRightIcon = () => {
-    const { Icon } = props
-    if (variant === 'icon-right' && Icon) {
-      return <Icon size={size} />
+    if (variant === 'icon-right') {
+      const { Icon } = props
+      return Icon && <Icon size={size} />
     }
-    return null
   }
 
-  return (
-    <a href={href} className={linkClasses}>
-      {renderLeftIcon()}
-      {text}
-      {renderRightIcon()}
-    </a>
-  )
+  const renderHyperlink = () => {
+    const hyperlinkType: 'a' | 'div' = !disabled ? 'a' : 'div'
+
+    const hrefAttribute = hyperlinkType === 'a' ? href : undefined
+
+    return createElement(
+      hyperlinkType,
+      {
+        className: twMerge(hyperlink({ color, size, variant, disabled })),
+        href: hrefAttribute,
+      },
+      renderLeftIcon(),
+      children,
+      renderRightIcon(),
+    )
+  }
+
+  return renderHyperlink()
 }
 
 export default CosHyperlink
