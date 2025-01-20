c5220db0cf6ad2bf9699928745ece7ba015937b0
steven.chiu <steven.chiu@bigstack.co>
2025-01-20 17:55:14 +0800

    style(ui-library): cos toggle story

--- packages/cube-frontend-ui-library/src/stories/components/CosToggle/ToggleRowHeader.tsx
+++ packages/cube-frontend-ui-library/src/stories/components/CosToggle/ToggleRowHeader.tsx
@@ -1,6 +1,6 @@
 export const ToggleRowHeader = () => {
   return (
-    <div className="grid grid-cols-6 justify-items-center gap-3">
+    <div className="grid grid-cols-[repeat(3,120px)] justify-items-center gap-3">
       <h3 />
       <h3 className="secondary-h3">Regular</h3>
       <h3 className="secondary-h3">With Label</h3>
