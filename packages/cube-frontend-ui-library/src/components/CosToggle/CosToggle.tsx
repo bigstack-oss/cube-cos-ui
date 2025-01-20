f33b7955221ae9d17383af9af60aed04a73f3141
steven.chiu <steven.chiu@bigstack.co>
2025-01-20 17:51:57 +0800

    feat(ui-library): trigger toggle by clicking label

--- packages/cube-frontend-ui-library/src/components/CosToggle/CosToggle.tsx
+++ packages/cube-frontend-ui-library/src/components/CosToggle/CosToggle.tsx
@@ -1,5 +1,5 @@
-import { cva } from 'class-variance-authority'
 import { twMerge } from 'tailwind-merge'
+import { labelCva, thumb, track } from './styles'
 
 export type CosToggleProps = {
   isOn: boolean
@@ -8,66 +8,17 @@ export type CosToggleProps = {
   onChange?: (isOn: boolean) => void
 }
 
-const track = cva(
-  'flex h-[20px] w-9 items-center rounded-full border border-transparent transition-colors',
-  {
-    variants: {
-      isOn: {
-        true: ['bg-status-positive'],
-        false: ['bg-functional-disable-text'],
-      },
-      disabled: {
-        false: ['cursor-pointer'],
-        true: ['cursor-default'],
-      },
-    },
-    compoundVariants: [
-      // Not disabled
-      {
-        isOn: true,
-        disabled: false,
-        className: ['hover:bg-green-400'],
-      },
-      {
-        isOn: false,
-        disabled: false,
-        className: ['hover:bg-grey-400'],
-      },
-      // Disabled
-      {
-        isOn: true,
-        disabled: true,
-        className: ['border-green-200 bg-green-100'],
-      },
-      {
-        isOn: false,
-        disabled: true,
-        className: ['border-functional-disable bg-grey-150'],
-      },
-    ],
-    defaultVariants: {
-      disabled: false,
-    },
-  },
-)
-
-const thumb = cva(
-  'size-[17px] rounded-full bg-grey-0 shadow-md transition-transform',
-  {
-    variants: {
-      isOn: {
-        true: ['translate-x-4'],
-        false: ['translate-x-px'],
-      },
-    },
-  },
-)
-
 export const CosToggle = (props: CosToggleProps) => {
-  const { isOn, label, disabled, onChange } = props
+  const { isOn, label, disabled = false, onChange } = props
 
-  const onTrackClick = () => {
+  const toggleIsOn = () => {
     onChange?.(!isOn)
+  }
+
+  const onLabelClick = () => {
+    if (!disabled) {
+      toggleIsOn()
+    }
   }
 
   return (
@@ -76,12 +27,14 @@ export const CosToggle = (props: CosToggleProps) => {
         type="button"
         className={twMerge(track({ isOn, disabled }))}
         disabled={disabled}
-        onClick={onTrackClick}
+        onClick={toggleIsOn}
       >
         <span className={thumb({ isOn })} />
       </button>
       {label && (
-        <label className="primary-body4 text-functional-text">{label}</label>
+        <label className={labelCva({ disabled })} onClick={onLabelClick}>
+          {label}
+        </label>
       )}
     </div>
   )
