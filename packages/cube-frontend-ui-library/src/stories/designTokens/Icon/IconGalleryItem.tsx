b58015ac17e76602893cd2ec048e9252dcceda10
steven.chiu <steven.chiu@bigstack.co>
2025-01-20 14:47:56 +0800

    style(ui-library): icon gallery item padding

--- packages/cube-frontend-ui-library/src/stories/designTokens/Icon/IconGalleryItem.tsx
+++ packages/cube-frontend-ui-library/src/stories/designTokens/Icon/IconGalleryItem.tsx
@@ -16,7 +16,7 @@ export const IconGalleryItem = (props: IconGalleryItemProps) => {
   const { name, size, className, Component, onIconClick } = props
 
   return (
-    <div className="flex h-24 w-36 flex-col items-center justify-start gap-y-2 border p-3 transition-colors hover:bg-grey-200">
+    <div className="flex h-24 w-36 flex-col items-center justify-start gap-y-2 border p-3 pt-4 transition-colors hover:bg-grey-200">
       <CosIconFrame size={size} className={className} onClick={onIconClick}>
         <Component />
       </CosIconFrame>
