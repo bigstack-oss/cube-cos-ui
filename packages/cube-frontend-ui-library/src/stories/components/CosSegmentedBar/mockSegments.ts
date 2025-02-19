394443be4da5c5b085f41b343fc7bceb2270a3a4
steven.chiu <steven.chiu@bigstack.co>
2025-02-19 12:06:43 +0800

    feat(storybook): stories for CosSegmentedBar

---
+++ packages/cube-frontend-ui-library/src/stories/components/CosSegmentedBar/mockSegments.ts
@@ -0,0 +1,62 @@
+import { Segment } from '@cube-frontend/ui-library'
+
+export const vmSummarySegments: Segment[] = [
+  {
+    color: 'fill-chart-2',
+    colCount: 51,
+  },
+  {
+    color: 'fill-chart-4',
+    colCount: 1,
+  },
+  {
+    color: 'fill-chart-3',
+    colCount: 1,
+  },
+  {
+    color: 'fill-chart-6',
+    colCount: 1,
+  },
+  {
+    color: 'fill-status-negative',
+    colCount: 1,
+  },
+]
+
+export const roleSummarySegments: Segment[] = [
+  {
+    color: 'fill-chart-1',
+    colCount: 1,
+  },
+  {
+    color: 'fill-chart-2',
+    colCount: 1,
+  },
+  {
+    color: 'fill-chart-3',
+    colCount: 1,
+  },
+]
+
+export const healthHistorySegments: Segment[] = [
+  {
+    color: 'fill-cosmos-secondary',
+    colCount: 21,
+  },
+  {
+    color: 'fill-status-negative',
+    colCount: 1,
+  },
+  {
+    color: 'fill-cosmos-secondary',
+    colCount: 3,
+  },
+  {
+    color: 'fill-status-negative',
+    colCount: 4,
+  },
+  {
+    color: 'fill-cosmos-secondary',
+    colCount: 1,
+  },
+]
