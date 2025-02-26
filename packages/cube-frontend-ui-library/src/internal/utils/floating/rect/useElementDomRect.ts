01d7207f6604ccfacdf6eccebdc060b18733825b
steven.chiu <steven.chiu@bigstack.co>
2025-02-26 17:51:56 +0800

    fix(ui-library): useFloating hook scroll dimensions calculation bug

--- packages/cube-frontend-ui-library/src/internal/utils/floating/rect/useElementDomRect.ts
+++ packages/cube-frontend-ui-library/src/internal/utils/floating/rect/useElementDomRect.ts
@@ -1,32 +1,49 @@
 import { debounce } from '@cube-frontend/utils'
-import { useEffect, useState } from 'react'
+import { RefObject, useCallback, useEffect, useState } from 'react'
 
 export const useElementDomRect = (
-  element: HTMLElement | null,
+  elementRef: RefObject<HTMLElement | null>,
+  scrollableRootSelector: string | undefined,
 ): DOMRect | undefined => {
   const [rect, setRect] = useState<DOMRect | undefined>()
 
+  const getScrollableRoot = useCallback((): Element | typeof window | null => {
+    if (scrollableRootSelector) {
+      const element = document.querySelector(scrollableRootSelector)
+      if (!element) {
+        console.warn(
+          `Cannot find element with selector ${scrollableRootSelector}`,
+        )
+      }
+      return element
+    } else {
+      return window
+    }
+  }, [scrollableRootSelector])
+
   useEffect(() => {
     const syncRect = () => {
-      setRect(element?.getBoundingClientRect())
+      setRect(elementRef.current?.getBoundingClientRect())
     }
 
     const debouncedSync = debounce(syncRect, 100)
 
     syncRect()
 
+    const scrollableRoot = getScrollableRoot()
+
     // `getBoundingClientRect` returns a `DOMRect` with the element's size and
     // position relative to the viewport.
     // Therefore, we need to call it again to get the updated rect after
     // scrolling and resizing.
-    window.addEventListener('scroll', debouncedSync)
-    window.addEventListener('resize', debouncedSync)
+    scrollableRoot?.addEventListener('scroll', debouncedSync)
+    scrollableRoot?.addEventListener('resize', debouncedSync)
 
     return () => {
-      window.removeEventListener('scroll', debouncedSync)
-      window.removeEventListener('resize', debouncedSync)
+      scrollableRoot?.removeEventListener('scroll', debouncedSync)
+      scrollableRoot?.removeEventListener('resize', debouncedSync)
     }
-  }, [element])
+  }, [elementRef, getScrollableRoot])
 
   return rect
 }
--- packages/cube-frontend-web-app/src/layout/Content.tsx
+++ packages/cube-frontend-web-app/src/layout/Content.tsx
@@ -1,10 +1,21 @@
+import { UseFloatingExternalContextProvider } from '@cube-frontend/ui-library'
 import { PropsWithChildren } from 'react'
+
+const containerId = 'cos-content-container'
 
 const Content = (props: PropsWithChildren) => {
   const { children } = props
+
   return (
-    <div className="h-[calc(100svh_-_54px)] overflow-auto px-5 py-3">
-      {children}
+    <div
+      id={containerId}
+      className="h-[calc(100svh_-_54px)] overflow-auto px-5 py-3"
+    >
+      <UseFloatingExternalContextProvider
+        scrollableRootSelector={`#${containerId}`}
+      >
+        {children}
+      </UseFloatingExternalContextProvider>
     </div>
   )
 }
