1579fefaa8887ff87d609f93178ea472ca9667ea
steven-chiu-bigstack <steven.chiu@bigstack.co>
2025-04-10 19:00:52 +0800

    feat(web-app): show inline error notifications in tunings page
    Signed-off-by: steven-chiu-bigstack <steven.chiu@bigstack.co>

--- packages/cube-frontend-web-app/src/pages/events/tunings/_components/tableCells/ActionCell.tsx
+++ packages/cube-frontend-web-app/src/pages/events/tunings/_components/tableCells/ActionCell.tsx
@@ -60,7 +60,7 @@ export const ActionCell = (props: ActionCellProps) => {
   }
 
   const renderResetButton = () => {
-    if (isModified) {
+    if (!isModified) {
       return (
         <IconActionButton
           Icon={ArrowRotateLeft}
@@ -86,7 +86,7 @@ export const ActionCell = (props: ActionCellProps) => {
         <span>
           <CosToggle
             isOn={row.enabled}
-            disabled={!isModified || isUpdating}
+            disabled={isUpdating}
             onChange={onToggleChange}
           />
         </span>
