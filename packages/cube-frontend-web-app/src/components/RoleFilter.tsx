step-security-bot/bigstack-oss_cube-cos-ui

feat: 198 node list page and 200 support files page

packages/cube-frontend-web-app/src/components/RoleFilter.tsx
@@ -1,0 +1,75 @@
+import { ChangeEvent, useState } from 'react'
+import { CosDropdown } from '@cube-frontend/ui-library'
+import { Role, roles } from '../utils/role'
+
+export type RoleFilterProps = {
+  selectedRoles: Role[]
+  handleRolesSelect: (roles: Role[]) => void
+}
+
+export const RoleFilter = (props: RoleFilterProps) => {
+  const { selectedRoles, handleRolesSelect } = props
+
+  const [searchValue, setSearchValue] = useState('')
+
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value)
+  }
+
+  const handleAllSelect = (checked: boolean) => {
+    if (checked) {
+      handleRolesSelect(roles)
+    } else {
+      handleRolesSelect([])
+    }
+  }
+
+  const handleClearRolesClick = () => {
+    handleRolesSelect([])
+  }
+
+  const handleRoleClick = (role: Role) => {
+    const selectedRoleSet = new Set(selectedRoles)
+
+    if (selectedRoleSet.has(role)) {
+      handleRolesSelect(selectedRoles.filter((sr) => sr !== role))
+    } else {
+      handleRolesSelect([...selectedRoles, role])
+    }
+  }
+
+  return (
+    <CosDropdown
+      type="search-checkbox"
+      variant="in-table"
+      selectedItems={selectedRoles}
+      searchValue={searchValue}
+      onSearchChange={handleSearchChange}
+      onAllCheckChange={handleAllSelect}
+      onClearClick={handleClearRolesClick}
+    >
+      <CosDropdown.Trigger placeholder="Select a Role">
+        {selectedRoles.length > 0 ? `Roles` : undefined}
+      </CosDropdown.Trigger>
+      <CosDropdown.Menu>
+        {roles.map((role) => {
+          if (
+            searchValue &&
+            !role.toLowerCase().includes(searchValue.toLowerCase())
+          ) {
+            return null
+          }
+          return (
+            <CosDropdown.Item
+              key={role}
+              item={role}
+              onClick={() => handleRoleClick(role)}
+            >
+              {role}
+            </CosDropdown.Item>
+          )
+        })}
+      </CosDropdown.Menu>
+    </CosDropdown>
+  )
+}

packages/cube-frontend-web-app/src/pages/maintenance/supportFiles/_components/SupportFilesFilters.tsx
@@ -1,0 +1,79 @@
+import { Dayjs } from 'dayjs'
+import { CosIconFrame, CosSearchBarFilter } from '@cube-frontend/ui-library'
+import XIcon from '@cube-frontend/ui-library/icons/monochrome/x.svg?react'
+import { Role } from '@cube-frontend/web-app/utils/role'
+import { RoleFilter } from '@cube-frontend/web-app/components/RoleFilter'
+import { DatePickerFilter } from './DatePickerFilter'
+
+export type SupportFilesFiltersProps = {
+  searchKeyword: string
+  handleSearchKeywordChange: (value: string) => void
+  handleSearchKeywordClear: () => void
+  selectedRoles: Role[]
+  handleRolesSelect: (roles: Role[]) => void
+  startDate?: Dayjs
+  endDate?: Dayjs
+  handleStartDateChange: (date?: Dayjs) => void
+  handleEndDateChange: (date?: Dayjs) => void
+}
+
+export const SupportFilesFilters = (props: SupportFilesFiltersProps) => {
+  const {
+    searchKeyword,
+    handleSearchKeywordChange,
+    handleSearchKeywordClear,
+    selectedRoles,
+    handleRolesSelect,
+    startDate,
+    endDate,
+    handleStartDateChange,
+    handleEndDateChange,
+  } = props
+
+  const handleClearAllFilter = () => {
+    handleRolesSelect([])
+    handleSearchKeywordClear()
+    handleStartDateChange(undefined)
+    handleEndDateChange(undefined)
+  }
+
+  const showClearAllFilter =
+    !!searchKeyword || selectedRoles.length > 0 || !!startDate || !!endDate
+
+  return (
+    <div className="flex items-center gap-x-3">
+      <div className="flex items-center gap-x-2">
+        <CosSearchBarFilter
+          className="w-[320px]"
+          value={searchKeyword}
+          onChange={(e) => handleSearchKeywordChange(e.target.value)}
+          onInputClear={handleSearchKeywordClear}
+          placeholder="Search"
+          showDropdown={false}
+        />
+        <RoleFilter
+          selectedRoles={selectedRoles}
+          handleRolesSelect={handleRolesSelect}
+        />
+        <DatePickerFilter
+          startDate={startDate}
+          endDate={endDate}
+          handleStartDateChange={handleStartDateChange}
+          handleEndDateChange={handleEndDateChange}
+        />
+      </div>
+      {showClearAllFilter && (
+        <>
+          <div className="w-px self-stretch bg-functional-border-divider" />
+          <CosIconFrame
+            className="cursor-pointer"
+            size="md"
+            onClick={handleClearAllFilter}
+          >
+            <XIcon className="icon-md-sm text-functional-text-light" />
+          </CosIconFrame>
+        </>
+      )}
+    </div>
+  )
+}

packages/cube-frontend-web-app/src/pages/node/NodeListPage.tsx
@@ -1,0 +1,145 @@
+import { useContext, useEffect, useState } from 'react'
+import {
+  Node,
+  NodesApiGetNodesRequest,
+  SupportFilesApiCreateSupportFilesRequest,
+} from '@cube-frontend/api'
+import {
+  CosButton,
+  CosGeneralPanel,
+  CosPagination,
+  DEFAULT_ITEMS_PER_PAGE,
+} from '@cube-frontend/ui-library'
+import { CosApiResponse } from '@cube-frontend/web-app/hooks/useCosRequest/cosRequestUtils'
+import { nodesApi, supportFilesApi } from '@cube-frontend/web-app/api/cosApi'
+import { useCosMutationRequest } from '@cube-frontend/web-app/hooks/useCosRequest/useCosMutationRequest'
+import { useDebounce } from '@cube-frontend/web-app/hooks/useDebounce'
+import { DataCenterContext } from '@cube-frontend/web-app/context/DataCenterContext'
+import { Role } from '@cube-frontend/web-app/utils/role'
+import { useCosGetRequest } from '@cube-frontend/web-app/hooks/useCosRequest/useCosGetRequest'
+import { NodeTable } from '@cube-frontend/web-app/components/NodeTable/NodeTable'
+import { NodeFilters } from './_components/NodeFilters'
+import { CreateSupportFilesModal } from './_components/CreateSupportFilesModal'
+
+export const NodeListPage = () => {
+  const dataCenter = useContext(DataCenterContext)
+
+  const [searchKeyword, setSearchKeyword] = useState('')
+  const [selectedRoles, setSelectedRoles] = useState<Role[]>([])
+  const [pageNum, setPageNum] = useState(1)
+  const [pageSize, setPageSize] = useState(DEFAULT_ITEMS_PER_PAGE)
+
+  const [debouncedSearchKeyword, setDebounceSearchKeyword] = useDebounce(
+    searchKeyword,
+    300,
+  )
+
+  const { data: nodesData, isLoading } = useCosGetRequest(
+    nodesApi.getNodes,
+    () => {
+      return {
+        dataCenter: dataCenter.name,
+        pageNum,
+        pageSize,
+
+        // TODO: add keyword search and roles filter to openAPI.
+        // @ts-expect-error: the API not supported yet.
+        roles: selectedRoles,
+        keyword: debouncedSearchKeyword,
+      } satisfies NodesApiGetNodesRequest
+    },
+  )
+
+  const handleSearchKeywordClear = () => {
+    setSearchKeyword('')
+    setDebounceSearchKeyword('')
+  }
+
+  const {
+    isLoading: isCreatingSupportFiles,
+    mutateResource: createSupportFiles,
+  } = useCosMutationRequest(
+    supportFilesApi.createSupportFiles as (
+      params: SupportFilesApiCreateSupportFilesRequest,
+    ) => Promise<CosApiResponse<undefined>>,
+  )
+
+  const [isCreateSupportFilesModalOpen, setIsCreateSupportFilesModalOpen] =
+    useState(false)
+  const [selectedNodes, setSelectedNodes] = useState<Node[]>([])
+  const [comments, setComments] = useState('')
+
+  // TODO: select all nodes for create support files modal,
+  // will remove this when table batch actions are implemented.
+  useEffect(() => {
+    setSelectedNodes(nodesData?.nodes || [])
+  }, [nodesData])
+
+  const handleCreateSupportFilesButtonClick = () => {
+    setComments('')
+    setIsCreateSupportFilesModalOpen(true)
+  }
+
+  const handleConfirmCreateSupportFiles = async () => {
+    try {
+      await createSupportFiles({
+        dataCenter: dataCenter.name,
+        createSupportFilesRequest: {
+          description: comments,
+          hosts: selectedNodes.map((node) => node.hostname),
+        },
+      })
+    } catch (error) {
+      console.error('Create support files error: ', error)
+    } finally {
+      setIsCreateSupportFilesModalOpen(false)
+      setSelectedNodes([])
+    }
+  }
+
+  return (
+    <>
+      <CosGeneralPanel topic="Nodes">
+        <div className="flex flex-col gap-y-3">
+          <div className="flex items-center justify-between">
+            <NodeFilters
+              searchKeyword={searchKeyword}
+              handleSearchKeywordChange={setSearchKeyword}
+              handleSearchKeywordClear={handleSearchKeywordClear}
+              selectedRoles={selectedRoles}
+              handleRolesSelect={setSelectedRoles}
+            />
+            <CosButton
+              onClick={handleCreateSupportFilesButtonClick}
+              disabled={selectedNodes.length === 0}
+            >
+              Create support files
+            </CosButton>
+          </div>
+          <NodeTable
+            rows={nodesData?.nodes || []}
+            isLoading={isLoading}
+            skeletonRowCount={pageSize}
+          />
+          <CosPagination
+            isLoading={isLoading}
+            totalItems={nodesData?.page.totalItemCount ?? 0}
+            currentPage={pageNum}
+            itemsPerPage={pageSize}
+            onPageChange={setPageNum}
+            onItemsPerPageChange={setPageSize}
+          />
+        </div>
+      </CosGeneralPanel>
+      <CreateSupportFilesModal
+        isOpen={isCreateSupportFilesModalOpen}
+        isCreating={isCreatingSupportFiles}
+        selectedNodes={selectedNodes}
+        comments={comments}
+        onCommentsChange={setComments}
+        onCreateClick={handleConfirmCreateSupportFiles}
+        onCloseClick={() => setIsCreateSupportFilesModalOpen(false)}
+      />
+    </>
+  )
+}

packages/cube-frontend-web-app/src/pages/node/_components/NodeFilters.tsx
@@ -1,0 +1,60 @@
+import { CosIconFrame, CosSearchBarFilter } from '@cube-frontend/ui-library'
+import XIcon from '@cube-frontend/ui-library/icons/monochrome/x.svg?react'
+import { Role } from '@cube-frontend/web-app/utils/role'
+import { RoleFilter } from '@cube-frontend/web-app/components/RoleFilter'
+
+export type NodeFiltersProps = {
+  searchKeyword: string
+  handleSearchKeywordChange: (value: string) => void
+  handleSearchKeywordClear: () => void
+  selectedRoles: Role[]
+  handleRolesSelect: (roles: Role[]) => void
+}
+
+export const NodeFilters = (props: NodeFiltersProps) => {
+  const {
+    searchKeyword,
+    handleSearchKeywordChange,
+    handleSearchKeywordClear,
+    selectedRoles,
+    handleRolesSelect,
+  } = props
+
+  const handleClearAllFilter = () => {
+    handleRolesSelect([])
+    handleSearchKeywordClear()
+  }
+
+  const showClearAllFilter = !!searchKeyword || selectedRoles.length > 0
+
+  return (
+    <div className="flex items-center gap-x-3">
+      <div className="flex items-center gap-x-2">
+        <CosSearchBarFilter
+          className="w-[320px]"
+          value={searchKeyword}
+          onChange={(e) => handleSearchKeywordChange(e.target.value)}
+          onInputClear={handleSearchKeywordClear}
+          placeholder="Search"
+          showDropdown={false}
+        />
+        <RoleFilter
+          selectedRoles={selectedRoles}
+          handleRolesSelect={handleRolesSelect}
+        />
+      </div>
+      {showClearAllFilter && (
+        <>
+          <div className="w-px self-stretch bg-functional-border-divider" />
+          <CosIconFrame
+            className="cursor-pointer"
+            size="md"
+            onClick={handleClearAllFilter}
+          >
+            <XIcon className="icon-md-sm text-functional-text-light" />
+          </CosIconFrame>
+        </>
+      )}
+    </div>
+  )
+}

bigstack-oss/cube-cos-ui

Signed-off-by: Raven Pan <raven.pan@bigstack.co>

packages/cube-frontend-web-app/src/components/RoleFilter.tsx
@@ -1,0 +1,77 @@
+import { ChangeEvent, useState } from 'react'
+import { CosDropdown } from '@cube-frontend/ui-library'
+import { GetNodesRoleEnum } from '@cube-frontend/api'
+
+const allRoles = Object.values(GetNodesRoleEnum)
+
+export type RoleFilterProps = {
+  selectedRoles: GetNodesRoleEnum[]
+  handleRolesSelect: (roles: GetNodesRoleEnum[]) => void
+}
+
+export const RoleFilter = (props: RoleFilterProps) => {
+  const { selectedRoles, handleRolesSelect } = props
+
+  const [searchValue, setSearchValue] = useState('')
+
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value)
+  }
+
+  const handleAllSelect = (checked: boolean) => {
+    if (checked) {
+      handleRolesSelect(allRoles)
+    } else {
+      handleRolesSelect([])
+    }
+  }
+
+  const handleClearRolesClick = () => {
+    handleRolesSelect([])
+  }
+
+  const handleRoleClick = (role: GetNodesRoleEnum) => {
+    const selectedRoleSet = new Set(selectedRoles)
+
+    if (selectedRoleSet.has(role)) {
+      handleRolesSelect(selectedRoles.filter((sr) => sr !== role))
+    } else {
+      handleRolesSelect([...selectedRoles, role])
+    }
+  }
+
+  return (
+    <CosDropdown
+      type="search-checkbox"
+      variant="in-table"
+      selectedItems={selectedRoles}
+      searchValue={searchValue}
+      onSearchChange={handleSearchChange}
+      onAllCheckChange={handleAllSelect}
+      onClearClick={handleClearRolesClick}
+    >
+      <CosDropdown.Trigger placeholder="Select a Role">
+        {selectedRoles.length > 0 ? `Roles` : undefined}
+      </CosDropdown.Trigger>
+      <CosDropdown.Menu>
+        {allRoles.map((role) => {
+          if (
+            searchValue &&
+            !role.toLowerCase().includes(searchValue.toLowerCase())
+          ) {
+            return null
+          }
+          return (
+            <CosDropdown.Item
+              key={role}
+              item={role}
+              onClick={() => handleRoleClick(role)}
+            >
+              {role}
+            </CosDropdown.Item>
+          )
+        })}
+      </CosDropdown.Menu>
+    </CosDropdown>
+  )
+}

packages/cube-frontend-web-app/src/pages/maintenance/supportFiles/_components/SupportFilesFilters.tsx
@@ -1,0 +1,79 @@
+import { Dayjs } from 'dayjs'
+import { CosIconFrame, CosSearchBarFilter } from '@cube-frontend/ui-library'
+import XIcon from '@cube-frontend/ui-library/icons/monochrome/x.svg?react'
+import { RoleFilter } from '@cube-frontend/web-app/components/RoleFilter'
+import { DatePickerFilter } from './DatePickerFilter'
+import { GetNodesRoleEnum } from '@cube-frontend/api'
+
+export type SupportFilesFiltersProps = {
+  searchKeyword: string
+  handleSearchKeywordChange: (value: string) => void
+  handleSearchKeywordClear: () => void
+  selectedRoles: GetNodesRoleEnum[]
+  handleRolesSelect: (roles: GetNodesRoleEnum[]) => void
+  startDate?: Dayjs
+  endDate?: Dayjs
+  handleStartDateChange: (date?: Dayjs) => void
+  handleEndDateChange: (date?: Dayjs) => void
+}
+
+export const SupportFilesFilters = (props: SupportFilesFiltersProps) => {
+  const {
+    searchKeyword,
+    handleSearchKeywordChange,
+    handleSearchKeywordClear,
+    selectedRoles,
+    handleRolesSelect,
+    startDate,
+    endDate,
+    handleStartDateChange,
+    handleEndDateChange,
+  } = props
+
+  const handleClearAllFilter = () => {
+    handleRolesSelect([])
+    handleSearchKeywordClear()
+    handleStartDateChange(undefined)
+    handleEndDateChange(undefined)
+  }
+
+  const showClearAllFilter =
+    !!searchKeyword || selectedRoles.length > 0 || !!startDate || !!endDate
+
+  return (
+    <div className="flex items-center gap-x-3">
+      <div className="flex items-center gap-x-2">
+        <CosSearchBarFilter
+          className="w-[320px]"
+          value={searchKeyword}
+          onChange={(e) => handleSearchKeywordChange(e.target.value)}
+          onInputClear={handleSearchKeywordClear}
+          placeholder="Search"
+          showDropdown={false}
+        />
+        <RoleFilter
+          selectedRoles={selectedRoles}
+          handleRolesSelect={handleRolesSelect}
+        />
+        <DatePickerFilter
+          startDate={startDate}
+          endDate={endDate}
+          handleStartDateChange={handleStartDateChange}
+          handleEndDateChange={handleEndDateChange}
+        />
+      </div>
+      {showClearAllFilter && (
+        <>
+          <div className="w-px self-stretch bg-functional-border-divider" />
+          <CosIconFrame
+            className="cursor-pointer"
+            size="md"
+            onClick={handleClearAllFilter}
+          >
+            <XIcon className="icon-md-sm text-functional-text-light" />
+          </CosIconFrame>
+        </>
+      )}
+    </div>
+  )
+}

packages/cube-frontend-web-app/src/pages/node/NodeListPage.tsx
@@ -1,0 +1,143 @@
+import { useContext, useEffect, useState } from 'react'
+import {
+  GetNodesRoleEnum,
+  Node,
+  NodesApiGetNodesRequest,
+  SupportFilesApiCreateSupportFilesRequest,
+} from '@cube-frontend/api'
+import {
+  CosButton,
+  CosGeneralPanel,
+  CosPagination,
+  DEFAULT_ITEMS_PER_PAGE,
+} from '@cube-frontend/ui-library'
+import { CosApiResponse } from '@cube-frontend/web-app/hooks/useCosRequest/cosRequestUtils'
+import { nodesApi, supportFilesApi } from '@cube-frontend/web-app/api/cosApi'
+import { useCosMutationRequest } from '@cube-frontend/web-app/hooks/useCosRequest/useCosMutationRequest'
+import { useDebounce } from '@cube-frontend/web-app/hooks/useDebounce'
+import { DataCenterContext } from '@cube-frontend/web-app/context/DataCenterContext'
+import { useCosGetRequest } from '@cube-frontend/web-app/hooks/useCosRequest/useCosGetRequest'
+import { NodeTable } from '@cube-frontend/web-app/components/NodeTable/NodeTable'
+import { NodeFilters } from './_components/NodeFilters'
+import { CreateSupportFilesModal } from './_components/CreateSupportFilesModal'
+
+export const NodeListPage = () => {
+  const dataCenter = useContext(DataCenterContext)
+
+  const [searchKeyword, setSearchKeyword] = useState('')
+  const [selectedRoles, setSelectedRoles] = useState<GetNodesRoleEnum[]>([])
+  const [pageNum, setPageNum] = useState(1)
+  const [pageSize, setPageSize] = useState(DEFAULT_ITEMS_PER_PAGE)
+
+  const [debouncedSearchKeyword, setDebounceSearchKeyword] = useDebounce(
+    searchKeyword,
+    300,
+  )
+
+  const { data: nodesData, isLoading } = useCosGetRequest(
+    nodesApi.getNodes,
+    () => {
+      return {
+        dataCenter: dataCenter.name,
+        pageNum,
+        pageSize,
+        // TODO: Waiting for the COS API support multiple roles filter.
+        role: selectedRoles[0],
+        keyword: debouncedSearchKeyword,
+      } satisfies NodesApiGetNodesRequest
+    },
+  )
+
+  const handleSearchKeywordClear = () => {
+    setSearchKeyword('')
+    setDebounceSearchKeyword('')
+  }
+
+  const {
+    isLoading: isCreatingSupportFiles,
+    mutateResource: createSupportFiles,
+  } = useCosMutationRequest(
+    supportFilesApi.createSupportFiles as (
+      params: SupportFilesApiCreateSupportFilesRequest,
+    ) => Promise<CosApiResponse<undefined>>,
+  )
+
+  const [isCreateSupportFilesModalOpen, setIsCreateSupportFilesModalOpen] =
+    useState(false)
+  const [selectedNodes, setSelectedNodes] = useState<Node[]>([])
+  const [comments, setComments] = useState('')
+
+  // TODO: select all nodes for create support files modal,
+  // will remove this when table batch actions are implemented.
+  useEffect(() => {
+    setSelectedNodes(nodesData?.nodes || [])
+  }, [nodesData])
+
+  const handleCreateSupportFilesButtonClick = () => {
+    setComments('')
+    setIsCreateSupportFilesModalOpen(true)
+  }
+
+  const handleConfirmCreateSupportFiles = async () => {
+    try {
+      await createSupportFiles({
+        dataCenter: dataCenter.name,
+        createSupportFilesRequest: {
+          description: comments,
+          hosts: selectedNodes.map((node) => node.hostname),
+        },
+      })
+    } catch (error) {
+      console.error('Create support files error: ', error)
+    } finally {
+      setIsCreateSupportFilesModalOpen(false)
+      setSelectedNodes([])
+    }
+  }
+
+  return (
+    <>
+      <CosGeneralPanel topic="Nodes">
+        <div className="flex flex-col gap-y-3">
+          <div className="flex items-center justify-between">
+            <NodeFilters
+              searchKeyword={searchKeyword}
+              handleSearchKeywordChange={setSearchKeyword}
+              handleSearchKeywordClear={handleSearchKeywordClear}
+              selectedRoles={selectedRoles}
+              handleRolesSelect={setSelectedRoles}
+            />
+            <CosButton
+              onClick={handleCreateSupportFilesButtonClick}
+              disabled={selectedNodes.length === 0}
+            >
+              Create support files
+            </CosButton>
+          </div>
+          <NodeTable
+            rows={nodesData?.nodes || []}
+            isLoading={isLoading}
+            skeletonRowCount={pageSize}
+          />
+          <CosPagination
+            isLoading={isLoading}
+            totalItems={nodesData?.page.totalItemCount ?? 0}
+            currentPage={pageNum}
+            itemsPerPage={pageSize}
+            onPageChange={setPageNum}
+            onItemsPerPageChange={setPageSize}
+          />
+        </div>
+      </CosGeneralPanel>
+      <CreateSupportFilesModal
+        isOpen={isCreateSupportFilesModalOpen}
+        isCreating={isCreatingSupportFiles}
+        selectedNodes={selectedNodes}
+        comments={comments}
+        onCommentsChange={setComments}
+        onCreateClick={handleConfirmCreateSupportFiles}
+        onCloseClick={() => setIsCreateSupportFilesModalOpen(false)}
+      />
+    </>
+  )
+}

packages/cube-frontend-web-app/src/pages/node/_components/NodeFilters.tsx
@@ -1,0 +1,60 @@
+import { CosIconFrame, CosSearchBarFilter } from '@cube-frontend/ui-library'
+import XIcon from '@cube-frontend/ui-library/icons/monochrome/x.svg?react'
+import { RoleFilter } from '@cube-frontend/web-app/components/RoleFilter'
+import { GetNodesRoleEnum } from '@cube-frontend/api'
+
+export type NodeFiltersProps = {
+  searchKeyword: string
+  handleSearchKeywordChange: (value: string) => void
+  handleSearchKeywordClear: () => void
+  selectedRoles: GetNodesRoleEnum[]
+  handleRolesSelect: (roles: GetNodesRoleEnum[]) => void
+}
+
+export const NodeFilters = (props: NodeFiltersProps) => {
+  const {
+    searchKeyword,
+    handleSearchKeywordChange,
+    handleSearchKeywordClear,
+    selectedRoles,
+    handleRolesSelect,
+  } = props
+
+  const handleClearAllFilter = () => {
+    handleRolesSelect([])
+    handleSearchKeywordClear()
+  }
+
+  const showClearAllFilter = !!searchKeyword || selectedRoles.length > 0
+
+  return (
+    <div className="flex items-center gap-x-3">
+      <div className="flex items-center gap-x-2">
+        <CosSearchBarFilter
+          className="w-[320px]"
+          value={searchKeyword}
+          onChange={(e) => handleSearchKeywordChange(e.target.value)}
+          onInputClear={handleSearchKeywordClear}
+          placeholder="Search"
+          showDropdown={false}
+        />
+        <RoleFilter
+          selectedRoles={selectedRoles}
+          handleRolesSelect={handleRolesSelect}
+        />
+      </div>
+      {showClearAllFilter && (
+        <>
+          <div className="w-px self-stretch bg-functional-border-divider" />
+          <CosIconFrame
+            className="cursor-pointer"
+            size="md"
+            onClick={handleClearAllFilter}
+          >
+            <XIcon className="icon-md-sm text-functional-text-light" />
+          </CosIconFrame>
+        </>
+      )}
+    </div>
+  )
+}