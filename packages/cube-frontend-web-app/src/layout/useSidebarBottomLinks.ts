a706c8be12ae8a9d1059ace5dc7401a10b0b613a
steven-chiu-bigstack <steven.chiu@bigstack.co>
2025-04-15 09:57:14 +0800

    fix(web-app): side bar help link
    Signed-off-by: steven-chiu-bigstack <steven.chiu@bigstack.co>

--- packages/cube-frontend-web-app/src/layout/useSidebarBottomLinks.ts
+++ packages/cube-frontend-web-app/src/layout/useSidebarBottomLinks.ts
@@ -1,10 +1,16 @@
 import { SideBarBottomLinkProps } from '@cube-frontend/ui-library'
+import { useContext } from 'react'
+import { DataCenterContext } from '../context/DataCenterContext'
 
 export const useSidebarBottomLinks = (): SideBarBottomLinkProps[] => {
+  const { additional } = useContext(DataCenterContext)
+
   const links: SideBarBottomLinkProps[] = [
     {
       text: 'Help',
-      href: 'https://docs.bigstack.co/docs/cubecos/quick_start/get_started',
+      href:
+        additional.helpUrl ||
+        'https://docs.bigstack.co/docs/cubecos/quick_start/get_started',
     },
   ]
 
