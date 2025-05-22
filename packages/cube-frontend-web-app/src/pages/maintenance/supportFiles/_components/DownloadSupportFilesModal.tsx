4005b9f3ba8c0f8efda561d64465ab9e688cdc85
Raven Pan <raven.pan@bigstack.co>
2025-05-22 17:30:46 +0800

    fix: 99 not able to download support files
    Signed-off-by: Raven Pan <raven.pan@bigstack.co>

--- packages/cube-frontend-web-app/src/pages/maintenance/supportFiles/_components/DownloadSupportFilesModal.tsx
+++ packages/cube-frontend-web-app/src/pages/maintenance/supportFiles/_components/DownloadSupportFilesModal.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { CosCheckbox, CosModal } from '@cube-frontend/ui-library'
 import { SupportFile } from '@cube-frontend/api'
 import { SupportFileRow } from '../MaintenanceSupportFilesPage'
-import { download } from '@cube-frontend/web-app/utils/download'
 import { formatSupportFilesTimestamp } from '@cube-frontend/web-app/utils/date'
 
 type DownloadSupportFilesModalProps = {
@@ -19,7 +18,7 @@ export const DownloadSupportFilesModal = (
   const [selectedFiles, setSelectedFiles] = useState<SupportFile[]>([])
 
   const handleDownload = () => {
-    selectedFiles.forEach((file) => download(file.url, file.name))
+    selectedFiles.forEach((file) => window.open(file.url, '_blank'))
     onCloseClick()
   }
 
