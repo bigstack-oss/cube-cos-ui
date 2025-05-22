243466b28374a6322396ebf63eec17adecd17647
Raven Pan <raven.pan@bigstack.co>
2025-05-22 15:26:54 +0800

    feat: test support files connection
    Signed-off-by: Raven Pan <raven.pan@bigstack.co>

--- packages/cube-frontend-web-app/vite.config.ts
+++ packages/cube-frontend-web-app/vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
   return {
     plugins: [
+      basicSsl(),
       react(),
       viteTsConfigPaths(),
       svgr({
