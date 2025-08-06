9136080b95767dde68499567bad205463094a54a
steven-chiu-bigstack <steven.chiu@bigstack.co>
2025-08-06 11:45:31 +0800

    style(web-app): node vip label
    Signed-off-by: steven-chiu-bigstack <steven.chiu@bigstack.co>

--- packages/cube-frontend-web-app/src/pages/node/_components/VipLabel.tsx
+++ packages/cube-frontend-web-app/src/pages/node/_components/VipLabel.tsx
@@ -1,7 +1,5 @@
+import { CosIconText } from '@cube-frontend/ui-library'
+
 export const VipLabel = () => {
-  return (
-    <span className="primary-body4 rounded bg-chart-6 px-1 py-0.5 text-grey-0">
-      VIP
-    </span>
-  )
+  return <CosIconText type="secondary">VIP</CosIconText>
 }
