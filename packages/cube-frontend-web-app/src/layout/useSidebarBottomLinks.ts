bf798fce93273f4abc2504460ea0e5c595cad239
raven-pan-bigstack <raven.pan@bigstack.co>
2025-09-10 21:34:10 +0800

    feat: 605 sidebar api document hyperlink
    Signed-off-by: raven-pan-bigstack <raven.pan@bigstack.co>

--- packages/cube-frontend-web-app/src/layout/useSidebarBottomLinks.ts
+++ packages/cube-frontend-web-app/src/layout/useSidebarBottomLinks.ts
@@ -6,12 +6,19 @@ import { quickStartUrl } from '../utils/help'
 export const useSidebarBottomLinks = (): SideBarBottomLinkProps[] => {
   const { dataCenter } = useContext(DataCenterContext)
 
-  const links: SideBarBottomLinkProps[] = [
-    {
-      text: 'Help',
-      href: dataCenter?.additional.helpUrl || quickStartUrl,
-    },
-  ]
+  const links: SideBarBottomLinkProps[] = []
+
+  if (dataCenter) {
+    links.push({
+      text: 'API document',
+      href: dataCenter.additional.v1ApiDocUrl,
+    })
+  }
+
+  links.push({
+    text: 'Help',
+    href: dataCenter?.additional.helpUrl || quickStartUrl,
+  })
 
   return links
 }
