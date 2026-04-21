1a289418ca445ff22737d02f83d584d01a13ab20
raven-pan-bigstack <raven.pan@bigstack.co>
2026-04-21 18:01:49 +0800

    chore: 799 upgrade typescript and prettier
    Signed-off-by: raven-pan-bigstack <raven.pan@bigstack.co>

--- packages/cube-frontend-web-app/src/components/TimeRangeDropdown/timeRangeUtils.ts
+++ packages/cube-frontend-web-app/src/components/TimeRangeDropdown/timeRangeUtils.ts
@@ -1,4 +1,3 @@
-import { ParseKeys } from 'i18next'
 import { useTranslation } from 'react-i18next'
 import { GetHealthHistoryPastEnum } from '@cube-frontend/api'
 
@@ -6,7 +5,7 @@ export const timeRanges = Object.values(GetHealthHistoryPastEnum)
 
 export type TimeRange = (typeof timeRanges)[number]
 
-export const useTimeRangeLabels = (): Record<TimeRange, ParseKeys> => {
+export const useTimeRangeLabels = (): Record<TimeRange, string> => {
   const { t } = useTranslation()
 
   return {
